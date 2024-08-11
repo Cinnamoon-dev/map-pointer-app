ef9c277f53191c38b3e9a7c2399c73670680483e
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-08-11 00:12:51 -0300

    feat: listing available devices

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,12 +1,32 @@
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native"
 import useBLE from "@/hooks/useBLE"
 import { useState } from "react";
+import { Device } from "react-native-ble-plx";
+
+type DeviceListProps = {
+    devices: Device[]
+}
+
+const DeviceList = (props: DeviceListProps) => {
+    return(
+        <View>
+            { props.devices.map((device) => 
+                <View>
+                    <Text>{device.id}</Text>
+                    <Text>{device.name}</Text>
+                </View>
+            )}
+        </View>
+    )
+}
 
 const MainScreen = () => {
     const {
         requestPermissions,
         scanForPeripherals,
-        allDevices
+        allDevices,
+        connectToDevice,
+        connectedDevice
     } = useBLE();
 
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
@@ -29,8 +49,17 @@ const MainScreen = () => {
 
     return(
         <View style={styles.container}>
-            <TouchableOpacity onPress={openModal}>
+            <TouchableOpacity onPress={openModal} style={styles.connectButton}>
                 <Text>Connect</Text>
+            </TouchableOpacity>
+
+            <View>
+                <Text>Device List</Text>
+                <DeviceList devices={allDevices}/>
+            </View>
+
+            <TouchableOpacity onPress={() => console.log(allDevices)} style={styles.connectButton}>
+                <Text>Show Devices</Text>
             </TouchableOpacity>
         </View>
     )
@@ -38,10 +67,14 @@ const MainScreen = () => {
 
 const styles = StyleSheet.create({
     container: {
-        display: "flex",
-        justifyContent: "center",
+        flex: 1,
+        justifyContent: "space-evenly",
         alignItems: "center",
-        height: 100
+    },
+    connectButton: {
+        backgroundColor: "#d4d4d4",
+        padding: 10,
+        borderRadius: 5,
     }
 })
 
