fa7e9ea2e273f6ffe27b075f86f4cc417766664d
Karen-Ribeiro <mrkarenpro@gmail.com>
2024-09-14 15:37:06 -0300

    feat: add color variables in hexadecimal to the styles folder

---
+++ styles/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+  primary: '#051d40',       // Cor principal
+  secondary: '#145da0',     // Cor secundária
+  tertiary: '#52acff',      // Terceira cor
+  quaternary: '#b1d4e0'     // Quarta cor
+};
+
+export default colors;
