94168477db2a8045d1cf46c8d59f2b0b6edd5bfa
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-30 17:16:11 -0300

    feat: limit points added to each question

--- data/questions.js
+++ data/questions.js
@@ -3,60 +3,110 @@ export default [
     id: 0,
     question: 'Qual é o estado brasileiro com a maior produção de café?',
     correctAnswer: 'Minas Gerais',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 1,
     question: 'Qual estado brasileiro possui o maior litoral do país?',
     correctAnswer: 'Bahia',
-  },
-  {
-    id: 2,
-    question: 'Qual é o estado conhecido por sua grande produção de soja?',
-    correctAnswer: 'Mato Grosso do Sul',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 3,
     question: 'Qual é o estado brasileiro conhecido por sua produção de petróleo e gás natural, localizado na região Norte?',
     correctAnswer: 'Pará',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 4,
     question: 'Qual estado é conhecido como o "berço do samba" ',
     correctAnswer: 'Rio de Janeiro',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 5,
     question: 'Qual estado brasileiro é o menor em termos de área e está localizado na região Nordeste?',
     correctAnswer: 'Sergipe',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 6,
     question: 'Qual estado brasileiro é o maior produtor de energia elétrica do país, devido à sua extensa rede de hidrelétricas?',
     correctAnswer: 'Rondônia',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 7,
     question: 'Qual estado é o lar do famoso festival de música chamado "Lollapalooza", que acontece anualmente em sua capital?',
     correctAnswer: 'São Paulo',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 8,
     question: 'Qual estado é famoso por suas festas juninas e tem uma cidade que é conhecida por um dos maiores e mais tradicionais eventos de São João?',
     correctAnswer: 'Paraíba',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 9,
     question: 'Qual estado brasileiro é famoso por suas tradições folclóricas, como o boi-bumbá?',
     correctAnswer: 'Amazonas',
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
   },
   {
     id: 10,
     question: 'Qual estado é famoso por suas tradições culturais ligadas ao teatro, e tem uma cidade conhecida por suas festas tradicionais e o reggae local?',
     correctAnswer: 'Maranhão',
-  },
-  {
-    id: 11,
-    question: 'Qual estado é o maior produtor de petróleo do Brasil?',
-    correctAnswer: 'Espírito Santo',
-  },
+    limits: {
+      upper_x: 120,
+      upper_y: 80,
+      lower_x: 110,
+      lower_y: 75,
+    }
+  }
 ]
