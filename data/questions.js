bc8c253594117b192cbd9da15117191704c8a2dc
Karen-Ribeiro <mrkarenpro@gmail.com>
2024-10-02 10:40:14 -0300

    feat: deleting questions

--- data/questions.js
+++ data/questions.js
@@ -22,18 +22,7 @@ export default [
     }
   },
   {
-    id: 3,
-    question: 'Qual é o estado brasileiro conhecido por sua produção de petróleo e gás natural, localizado na região Norte?',
-    correctAnswer: 'Pará',
-    limits: {
-      upper_x: 120,
-      upper_y: 80,
-      lower_x: 110,
-      lower_y: 75,
-    }
-  },
-  {
-    id: 4,
+    id: 2,
     question: 'Qual estado é conhecido como o "berço do samba" ',
     correctAnswer: 'Rio de Janeiro',
     limits: {
@@ -44,29 +33,7 @@ export default [
     }
   },
   {
-    id: 5,
-    question: 'Qual estado brasileiro é o menor em termos de área e está localizado na região Nordeste?',
-    correctAnswer: 'Sergipe',
-    limits: {
-      upper_x: 120,
-      upper_y: 80,
-      lower_x: 110,
-      lower_y: 75,
-    }
-  },
-  {
-    id: 6,
-    question: 'Qual estado brasileiro é o maior produtor de energia elétrica do país, devido à sua extensa rede de hidrelétricas?',
-    correctAnswer: 'Rondônia',
-    limits: {
-      upper_x: 120,
-      upper_y: 80,
-      lower_x: 110,
-      lower_y: 75,
-    }
-  },
-  {
-    id: 7,
+    id: 3,
     question: 'Qual estado é o lar do famoso festival de música chamado "Lollapalooza", que acontece anualmente em sua capital?',
     correctAnswer: 'São Paulo',
     limits: {
@@ -77,7 +44,7 @@ export default [
     }
   },
   {
-    id: 8,
+    id: 4,
     question: 'Qual estado é famoso por suas festas juninas e tem uma cidade que é conhecida por um dos maiores e mais tradicionais eventos de São João?',
     correctAnswer: 'Paraíba',
     limits: {
@@ -88,18 +55,7 @@ export default [
     }
   },
   {
-    id: 9,
-    question: 'Qual estado brasileiro é famoso por suas tradições folclóricas, como o boi-bumbá?',
-    correctAnswer: 'Amazonas',
-    limits: {
-      upper_x: 120,
-      upper_y: 80,
-      lower_x: 110,
-      lower_y: 75,
-    }
-  },
-  {
-    id: 10,
+    id: 5,
     question: 'Qual estado é famoso por suas tradições culturais ligadas ao teatro, e tem uma cidade conhecida por suas festas tradicionais e o reggae local?',
     correctAnswer: 'Maranhão',
     limits: {
