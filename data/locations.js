959cf500aadb0307005c0c3e4ce4fa3bf413bc0b
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-10-02 21:38:28 -0300

    feat: location data template created

---
+++ data/locations.js
@@ -0,0 +1,191 @@
+export default [
+    {
+        estate: "Acre",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Alagoas",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Amapá",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Amazonas",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Bahia",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Ceará",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Distrito Federal",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Espírito santo",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Goiás",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Maranhão",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Mato Grosso",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Mato Grosso do Sul",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Minas Gerais",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Pará",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Paraíba",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Paraná",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Pernambuco",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Piauí",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Rio de Janeiro",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Rio Grande do Norte",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Rio Grande do Sul",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Rondônia",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Roraima",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Santa Catarina",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "São Paulo",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Sergipe",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    },
+    {
+        estate: "Tocantins",
+        upperX: 0,
+        upperY: 0,
+        lowerX: 0,
+        lowerY: 0
+    }
+]
